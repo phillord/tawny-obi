e4cbc5b085769fafb893792014f41d010fafcf72
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-05-31 07:47:18 +0100

    Test selector added. Whitespace.

--- project.clj
+++ project.clj
@@ -1,18 +1,23 @@
 (defproject uk.org.russet/tawny-obi "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
 
-  
+
   :license {:name "LGPL"
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :distribution :repo}
 
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [uk.org.russet/tawny-owl "0.12-SNAPSHOT"]
-                 
+
                  [net.sourceforge.owlapi/owlapi-api "3.4.4"]
 
-                 
+
                  ]
+
+
+  :test-selectors {:default (constantly true)
+                   :light (complement :heavy)}
+
+
   ;; this is a problem -- I have this namespace elsewhere
   :main tawny.core)
-
